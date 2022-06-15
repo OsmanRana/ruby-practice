f7f00ee7fd6121011114c2b0afc63defda44cd76
OsmanRana <osmancmc@yahoo.com>
2022-06-15 18:03:06 +0600

    initail commit

---
+++ hello.rb
@@ -0,0 +1,33 @@
+print "Hello Osman!\n"
+print "Good Buy!\n" # my first Ruby comment
+
+=begin
+This is a 
+multiline comment
+=end
+
+MYNAME =  "Osman\n"
+print MYNAME
+a = 10
+b = 20
+c = 30
+
+a, b, c = 10, 20, 30
+
+print a
+print b
+print c
+
+print a.kind_of? Integer
+print a.class
+
+name = "Osman"
+
+print name.class
+
+print a.to_s(2)
+print a.to_f
+
+print b.to_s
+
+print defined? a
---
+++ variables.rb
@@ -0,0 +1,8 @@
+=begin
+4 types of variable scope
+$global variable scope
+local variable or _LocalVariable scope
+@instance variable scope
+@@class variable scope
+CONSTANT
+=end
