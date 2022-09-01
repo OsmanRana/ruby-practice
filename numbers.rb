f1f2c4f2d0dbfd062a7f1d63cc4a927a42def80e
OsmanRana <osmancmc@yahoo.com>
2022-09-01 12:12:30 +0600

    numbers

---
+++ numbers.rb
@@ -0,0 +1,41 @@
+=begin
+Numbers:
+Two types 
+A. Integers: Whole nimbers such as 10
+B. Floats: Numbers that contains decimal point such as  4.5
+
+Arithmatic operation of two integers will asways return an integer
+17/4 #=>  4, not 4.25
+=end
+=begin
+print 1+1
+print 2-1
+print 2*3
+print 6/2
+print 5%2
+print 17/4.0
+=end
+
+=begin
+Converting Number Types:
+=end
+
+=begin
+Integer to Float
+=end
+print 13.to_f
+
+=begin
+Float to Integerr
+=end
+print 13.5.to_i
+
+=begin
+Even
+=end
+print 5.even?
+=begin
+Odd
+=end
+print 3.odd?
+
